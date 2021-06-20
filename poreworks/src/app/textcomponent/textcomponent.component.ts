57d9fdbe3e76862d24a24debdf3443684733b27b
mcube25 <mcube3567@gmail.com>
2021-06-20 16:57:14 -0700

    logo added

--- poreworks/src/app/textcomponent/textcomponent.component.ts
+++ poreworks/src/app/textcomponent/textcomponent.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {RecordsService} from '../records.servie'
+import {RecordsService} from '../records.service'
 
 @Component({
   selector: 'app-textcomponent',
