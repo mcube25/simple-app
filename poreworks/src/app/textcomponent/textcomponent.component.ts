8ed8cc30894c9ad6890293ab8f697cb77772ca01
mcube25 <mcube3567@gmail.com>
2021-06-20 15:24:52 -0700

    depency injection into component'

--- poreworks/src/app/textcomponent/textcomponent.component.ts
+++ poreworks/src/app/textcomponent/textcomponent.component.ts
@@ -1,13 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import {RecordsService} from '../records.servie'
 
 @Component({
   selector: 'app-textcomponent',
   templateUrl: './textcomponent.component.html',
-  styleUrls: ['./textcomponent.component.css']
+  styleUrls: ['./textcomponent.component.css'],
+  providers: [RecordsService]
 })
 export class TextcomponentComponent implements OnInit {
 
-  constructor() { }
+infoReceived1: string[] = [];
+infoReceived2: string[] = [];
+infoReceived3: string[] = [];
+//add services using dependency injection
+//create an object in the constructor to access the services
+getInfoFromSErviceClass1(){
+  this.infoReceived1 = this.rservice.getInfo1()
+}
+getInfoFromSErviceClass2(){
+  this.infoReceived2 = this.rservice.getInfo2()
+}
+getInfoFromSErviceClass3(){
+  this.infoReceived3 = this.rservice.getInfo3()
+}
+
+  constructor(private rservice: RecordsService) { }
 
   ngOnInit(): void {
   }
