2c4033aed278236c86ef5da03dd191ffdf3c32f2
mcube25 <mcube3567@gmail.com>
2021-06-20 15:22:53 -0700

    added services

--- poreworks/src/app/records.service.ts
+++ poreworks/src/app/records.service.ts
@@ -6,6 +6,17 @@ import { Injectable } from '@angular/core';
 export class RecordsService {
 info1: string[] = ['adam taylor','234', 'staff']
 info2: string[] = ["smith b", "456", "dre@b.com"]
+info3: string[] = ["abdul a", "786", "sbd@m.com"]
 
+//create method to get the info
+getInfo1(): string[]{
+  return this.info1
+}
+getInfo2(): string[]{
+  return this.info2
+}
+getInfo3(): string[]{
+  return this.info3
+}
   constructor() { }
 }
